317e3edefd3e677a8c8a668c036bae5e457d69d4
noonquiver <camilo.hernandez25@hotmail.com>
2022-08-03 19:20:48 -0500

    first commit

---
+++ Swifty Ninja/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  Swifty Ninja
+//
+//  Created by Camilo Hernández Guerrero on 3/08/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+}
