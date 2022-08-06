b6502eac5c40c3e1c4ad9a1cf03063b6770f63b1
noonquiver <camilo.hernandez25@hotmail.com>
2022-08-05 20:55:50 -0500

    Background, score, lives and slices functions created.

--- Swifty Ninja/GameScene.swift
+++ Swifty Ninja/GameScene.swift
@@ -8,6 +8,66 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var gameScore: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    var lives = 3
+    var livesImages = [SKSpriteNode]()
+    
+    var activeSliceForeground: SKShapeNode!
+    var activeSliceBackground: SKShapeNode!
+    
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
+    }
+    
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        
+        gameScore.position = CGPoint(x: 8, y: 8)
+        score = 0
+    }
+    
+    func createLives() {
+        for i in 0..<3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + i * 70), y: 720)
+            addChild(spriteNode)
+            livesImages.append(spriteNode)
+        }
+    }
+    
+    func createSlices() {
+        activeSliceForeground = SKShapeNode()
+        activeSliceForeground.zPosition = 3
+        activeSliceForeground.strokeColor = UIColor.white
+        activeSliceForeground.lineWidth = 5
+        
+        activeSliceBackground = SKShapeNode()
+        activeSliceBackground.zPosition = 2
+        activeSliceBackground.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBackground.lineWidth = 9
+        
+        addChild(activeSliceForeground)
+        addChild(activeSliceBackground)
     }
 }
